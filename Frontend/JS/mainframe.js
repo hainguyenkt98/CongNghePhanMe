f21fec2604d3bf166e65dd165e2bddc0dcd18a3f
DESKTOP-GIUI4CL\Hai Nguyen <hainguyenkt98@gmail.com>
2018-10-05 02:16:05 +0700

    them khung online, toggle, khung online để ttối tao về làm tiếp, tụi bay cứ dựa khung đó làm lên

--- Frontend/JS/mainframe.js
+++ Frontend/JS/mainframe.js
@@ -3,4 +3,8 @@ $(function(){
  		$('.sidebar').removeClass("sidebar-selected");
  		$(this).addClass("sidebar-selected");
  	});
+ 	$('.toggle-wrap').on('click', function() {
+    $(this).toggleClass('active');
+    $('aside').animate({width: 'toggle'}, 200);
+  });
 })  
